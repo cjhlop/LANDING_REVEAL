59e7ae734297a7d1ec4b5d1b5829e4a508222c7e
[dyad] <vasyl.k@demandsense.com>
2025-08-23 17:35:09 +0300

    [dyad] Removed all sections except hero from Index page - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,9 @@
 import { HeroV2 } from "@/components/HeroV2";
-import { Logos } from "@/components/Logos";
-import { Features } from "@/components/Features";
-import { Pricing } from "@/components/Pricing";
-import { Testimonials } from "@/components/Testimonials";
 
 const Index = () => {
   return (
     <>
       <HeroV2 />
-      <Logos />
-      <Features />
-      <Pricing />
-      <Testimonials />
     </>
   );
 };
