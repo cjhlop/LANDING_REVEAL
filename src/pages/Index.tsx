79bdc4cea8d0b9d478a4da64065a5b37a3dae4a5
[dyad] <vasyl.k@demandsense.com>
2025-08-27 11:40:53 +0300

    [dyad] Wrap PalmUI preview with dynamic border and remove separate showcase section - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,14 +2,12 @@ import Navbar from "@/components/Navbar";
 import { HeroV2 } from "@/components/HeroV2";
 import { MeetOurCustomers } from "@/components/customers";
 import { fallbackCustomerLogos } from "@/data/customerLogos";
-import Showcase from "@/components/Showcase";
 
 const Index = () => {
   return (
     <>
       <Navbar />
       <HeroV2 />
-      <Showcase />
       <MeetOurCustomers 
         logos={fallbackCustomerLogos}
         className="bg-gray-50"
