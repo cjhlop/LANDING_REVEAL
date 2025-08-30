7b842d385bbf853ba1d909e4abce6cd873f3159d
[dyad] <vasyl.k@demandsense.com>
2025-08-30 17:26:09 +0300

    [dyad] Fixed navbar logo height to render reliably - wrote 1 file(s)

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -5,11 +5,9 @@ const Logo: React.FC = () => {
   const [isLoaded, setIsLoaded] = React.useState(false);
 
   React.useEffect(() => {
-    // Trigger animation after component mounts
     const timer = setTimeout(() => {
       setIsLoaded(true);
     }, 100);
-
     return () => clearTimeout(timer);
   }, []);
 
@@ -19,37 +17,26 @@ const Logo: React.FC = () => {
       aria-label="Demand Sense home"
       className="inline-flex items-center justify-center select-none gap-2"
     >
-      {/* Logo.svg - slides up from bottom - reduced by 25% (32px -> 24px) */}
+      {/* Logo icon (24px height) */}
       <div
         className={`logo-icon transition-all duration-700 ease-out ${
-          isLoaded 
-            ? "translate-y-0 opacity-100" 
-            : "translate-y-8 opacity-0"
+          isLoaded ? "translate-y-0 opacity-100" : "translate-y-8 opacity-0"
         }`}
       >
-        <img
-          src="/logo.svg"
-          alt="Logo"
-          className="h-6 w-auto"
-          loading="eager"
-        />
+        <img src="/logo.svg" alt="Logo" className="h-6 w-auto" loading="eager" />
       </div>
 
-      {/* Demand Sense.svg - slides in from left with delay - reduced by 25% (24px -> 18px) */}
+      {/* Wordmark (18px height) */}
       <div
         className={`logo-text transition-all duration-700 ease-out ${
-          isLoaded 
-            ? "translate-x-0 opacity-100" 
-            : "-translate-x-4 opacity-0"
+          isLoaded ? "translate-x-0 opacity-100" : "-translate-x-4 opacity-0"
         }`}
-        style={{
-          transitionDelay: isLoaded ? "300ms" : "0ms"
-        }}
+        style={{ transitionDelay: isLoaded ? "300ms" : "0ms" }}
       >
         <img
           src="/demand-sense.svg"
           alt="Demand Sense"
-          className="h-4.5 w-auto"
+          className="h-[18px] w-auto"
           loading="eager"
         />
       </div>
