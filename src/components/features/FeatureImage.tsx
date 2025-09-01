855c49a73d65df88685a1676f71eeab4dbb408cd
[dyad] <vasyl.k@demandsense.com>
2025-09-01 10:54:36 +0300

    Reverted all changes back to version 119402e0f44f68a1e5b2c232efa92598792fa46a

--- src/components/features/FeatureImage.tsx
+++ src/components/features/FeatureImage.tsx
@@ -8,7 +8,14 @@ type Props = {
   position?: "left" | "right";
 };
 
-const FeatureImage: React.FC<Props> = ({ src, alt, className }) => {
+const FeatureImage: React.FC<Props> = ({ src, alt, className, position = "left" }) => {
+  // Use transform-origin to control the direction of the scale.
+  // An image on the left will scale from its right edge (origin-right), expanding leftwards.
+  // An image on the right will scale from its left edge (origin-left), expanding rightwards.
+  const transformClasses = position === 'left' 
+    ? 'lg:scale-[3] fhd:scale-[2.25] lg:origin-right' 
+    : 'lg:scale-[3] fhd:scale-[2.25] lg:origin-left';
+
   return (
     <div
       className={cn("relative magic-border", className)}
@@ -19,7 +26,10 @@ const FeatureImage: React.FC<Props> = ({ src, alt, className }) => {
         <img
           src={src}
           alt={alt}
-          className="w-full h-auto"
+          className={cn(
+            "w-full h-auto transition-transform duration-500 ease-out",
+            transformClasses
+          )}
           loading="lazy"
         />
       </div>
