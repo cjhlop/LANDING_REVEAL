ab1c3b732cc2d174d6ca6ad9cb3f9f5bcc808d2c
[dyad] <vasyl.k@demandsense.com>
2025-09-01 10:51:52 +0300

    Reverted all changes back to version 2f086a980782a71be6f1b0730456f6484df5a466

--- src/components/features/FeatureImage.tsx
+++ src/components/features/FeatureImage.tsx
@@ -8,21 +8,25 @@ type Props = {
   position?: "left" | "right";
 };
 
-const FeatureImage: React.FC<Props> = ({ src, alt, className }) => {
+const FeatureImage: React.FC<Props> = ({ src, alt, className, position = "left" }) => {
+  // Use transform-origin to control the direction of the scale.
+  // An image on the left will scale from its right edge (origin-right), expanding leftwards.
+  // An image on the right will scale from its left edge (origin-left), expanding rightwards.
+  const transformClasses = position === 'left' 
+    ? 'lg:scale-[3] fhd:scale-[2.25] lg:origin-right' 
+    : 'lg:scale-[3] fhd:scale-[2.25] lg:origin-left';
+
   return (
-    <div
-      className={cn("relative magic-border", className)}
-      // The border-radius for rounded-lg is 0.5rem. We set it here for the magic-border effect.
-      style={{ '--magic-radius': '0.5rem' } as React.CSSProperties}
-    >
-      <div className="relative z-[1] w-full h-full rounded-lg overflow-hidden ring-1 ring-gray-900/10">
-        <img
-          src={src}
-          alt={alt}
-          className="w-full h-auto"
-          loading="lazy"
-        />
-      </div>
+    <div className={cn("relative", className)}>
+      <img
+        src={src}
+        alt={alt}
+        className={cn(
+          "w-full h-auto rounded-lg ring-1 ring-gray-900/10 transition-transform duration-500 ease-out",
+          transformClasses
+        )}
+        loading="lazy"
+      />
     </div>
   );
 };
