baff9f36ed6c9b5ea9d5ef7303cb1573cfebe247
[dyad] <vasyl.k@demandsense.com>
2025-09-13 22:46:19 +0300

    [dyad] Add icons and center left content while preserving CardSwap layout - wrote 1 file(s)

--- src/components/CardSwapSection.tsx
+++ src/components/CardSwapSection.tsx
@@ -2,48 +2,60 @@ import React from 'react';
 import CardSwap, { Card } from './CardSwap';
 import BrowserHeader from '@/components/BrowserHeader';
 import ButtonGroup from '@/components/ButtonGroup';
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid';
 
 const CardSwapSection = () => {
   return (
     <section className="bg-gray-50 relative overflow-hidden" style={{ height: '900px' }}>
-      {/* Left content block */}
-      <div className="absolute inset-y-0 left-0 flex items-center">
-        <div className="px-6 md:px-12 max-w-[560px]">
-          <p className="text-[12px] leading-5 tracking-[1.2px] uppercase text-[#7C7C7C] font-['DM Mono']">
-            Deploy faster
-          </p>
-          <h3 className="mt-2 text-[32px] leading-[120%] tracking-[-0.9px] text-black font-medium">
-            A better workflow
-          </h3>
-          <p className="mt-4 text-[16px] leading-[150%] tracking-[-0.3px] text-[#7C7C7C]">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-            iste dolor cupiditate blanditiis ratione.
-          </p>
+      {/* Left content block in a centered 1216px container */}
+      <div className="absolute inset-y-0 left-0 right-0">
+        <div className="relative h-full w-full max-w-[1216px] mx-auto flex items-center">
+          <div className="px-6 md:px-12 max-w-[560px]">
+            <p className="text-[12px] leading-5 tracking-[1.2px] uppercase text-[#7C7C7C] font-['DM Mono']">
+              Deploy faster
+            </p>
+            <h3 className="mt-2 text-[32px] leading-[120%] tracking-[-0.9px] text-black font-medium">
+              A better workflow
+            </h3>
+            <p className="mt-4 text-[16px] leading-[150%] tracking-[-0.3px] text-[#7C7C7C]">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+              iste dolor cupiditate blanditiis ratione.
+            </p>
 
-          <dl className="mt-6 space-y-6 text-[16px] leading-[150%]">
-            <div>
-              <dt className="inline font-medium text-gray-900">Push to deploy.</dt>{' '}
-              <dd className="inline text-[#7C7C7C]">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
-              </dd>
-            </div>
-            <div>
-              <dt className="inline font-medium text-gray-900">SSL certificates.</dt>{' '}
-              <dd className="inline text-[#7C7C7C]">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-              </dd>
-            </div>
-            <div>
-              <dt className="inline font-medium text-gray-900">Database backups.</dt>{' '}
-              <dd className="inline text-[#7C7C7C]">
-                Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-              </dd>
-            </div>
-          </dl>
+            <dl className="mt-6 space-y-6 text-[16px] leading-[150%]">
+              <div className="relative pl-9">
+                <dt className="inline font-medium text-gray-900">
+                  <CloudArrowUpIcon aria-hidden="true" className="absolute top-1 left-1 h-5 w-5 text-[#3875f6]" />
+                  Push to deploy.
+                </dt>{' '}
+                <dd className="inline text-[#7C7C7C]">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                  iste dolor cupiditate blanditiis ratione.
+                </dd>
+              </div>
+              <div className="relative pl-9">
+                <dt className="inline font-medium text-gray-900">
+                  <LockClosedIcon aria-hidden="true" className="absolute top-1 left-1 h-5 w-5 text-[#3875f6]" />
+                  SSL certificates.
+                </dt>{' '}
+                <dd className="inline text-[#7C7C7C]">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                </dd>
+              </div>
+              <div className="relative pl-9">
+                <dt className="inline font-medium text-gray-900">
+                  <ServerIcon aria-hidden="true" className="absolute top-1 left-1 h-5 w-5 text-[#3875f6]" />
+                  Database backups.
+                </dt>{' '}
+                <dd className="inline text-[#7C7C7C]">
+                  Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
+                </dd>
+              </div>
+            </dl>
 
-          <div className="mt-8">
-            <ButtonGroup primaryLabel="Get started" secondaryLabel="Learn more" />
+            <div className="mt-8">
+              <ButtonGroup primaryLabel="Get started" secondaryLabel="Learn more" />
+            </div>
           </div>
         </div>
       </div>
