8097f83591dfd5ce26fd94e280372678d3502b4d
[dyad] <vasyl.k@demandsense.com>
2025-10-23 18:18:51 +0300

    Reverted all changes back to version 9458cf52f91b7a50a3b460dad75f00691068021c

--- src/components/pricing-main/index.ts
+++ src/components/pricing-main/index.ts
@@ -1,4 +1,2 @@
 export { default as PricingSection } from "./PricingSection";
-export { default as PricingCard } from "./PricingCard";
-export { default as PricingComparisonTable } from "./PricingComparisonTable";
-export { default as ProWaitlistModal } from "./ProWaitlistModal";
+export type { PricingSectionProps } from "./PricingSection";
