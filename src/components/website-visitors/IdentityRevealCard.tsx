6e23013133afbd82c64fa9a6fe89cf2f4adbf0a3
[dyad] <vasyl.k@demandsense.com>
2025-11-24 17:23:55 +0200

    [dyad] Doubling Sarah Jenkins card width - wrote 2 file(s)

--- src/components/website-visitors/IdentityRevealCard.tsx
+++ src/components/website-visitors/IdentityRevealCard.tsx
@@ -18,9 +18,9 @@ const IdentityRevealCard = ({ active }: { active: boolean }) => {
 
   return (
     <div className="relative group perspective-1000 z-50">
-      {/* Card Container - Compact Width (w-80) */}
+      {/* Card Container - Doubled Width (w-[40rem]) */}
       <div className={cn(
-        "w-80 bg-white rounded-xl shadow-[0_20px_50px_-12px_rgba(0,0,0,0.15)] border border-gray-100 overflow-hidden transition-all duration-700 ease-out transform",
+        "w-[40rem] bg-white rounded-xl shadow-[0_20px_50px_-12px_rgba(0,0,0,0.15)] border border-gray-100 overflow-hidden transition-all duration-700 ease-out transform",
         active ? "translate-y-0 opacity-100 rotate-x-0" : "translate-y-8 opacity-0 rotate-x-6"
       )}>
         
