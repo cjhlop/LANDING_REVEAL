6543e950814098a6775024160b265c90468bbada
[dyad] <vasyl.k@demandsense.com>
2025-12-29 10:30:02 +0200

    [dyad] Fixing the missing Badge component import in LinkedInAdsOptimization. - wrote 1 file(s)

--- src/components/LinkedInAdsOptimization.tsx
+++ src/components/LinkedInAdsOptimization.tsx
@@ -15,6 +15,7 @@ import {
   Target
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import SectionBadge from "./SectionBadge";
 
 type Feature = {
