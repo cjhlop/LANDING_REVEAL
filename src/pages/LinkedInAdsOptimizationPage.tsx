4faf07068f190bc3a3e373bd7c6b1a685175412e
[dyad] <artem@respect.studio>
2026-03-03 14:29:45 +0200

    [dyad] Updated Budget Control screenshot - wrote 1 file(s)

--- src/pages/LinkedInAdsOptimizationPage.tsx
+++ src/pages/LinkedInAdsOptimizationPage.tsx
@@ -376,7 +376,7 @@ const LinkedInAdsOptimizationPage = () => {
                 <div className="magic-border h-full w-full scale-105 lg:scale-110 origin-center" style={{ "--magic-radius": "1.5rem" } as React.CSSProperties}>
                   <div className="rounded-[inherit] border border-gray-200/80 shadow-2xl overflow-hidden bg-white ring-1 ring-black/5">
                     <img 
-                      src="dyad-media://media/LANDING_REVEAL/.dyad/media/465b04548e6ffd63318051e4ace6701b.png" 
+                      src="dyad-media://media/LANDING_REVEAL/.dyad/media/72bbb798f3beb4e01b106313a1b7e64a.png" 
                       alt="DemandSense LinkedIn Budget Control and Guardrails" 
                       className="w-full h-auto object-cover"
                     />
