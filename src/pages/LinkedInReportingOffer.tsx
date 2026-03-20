1a0f538a1070e0f95db78026aa14c7f54d583e95
[dyad] <artem@respect.studio>
2026-03-20 19:13:57 +0200

    [dyad] Fixing import paths to avoid case-sensitivity issues - wrote 1 file(s)

--- src/pages/LinkedInReportingOffer.tsx
+++ src/pages/LinkedInReportingOffer.tsx
@@ -3,7 +3,8 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { Logo, GetAccessDialog } from "@/components/navbar";
+import Logo from "@/components/navbar/Logo";
+import GetAccessDialog from "@/components/navbar/GetAccessDialog";
 import { 
   Check, 
   BarChart2, 
