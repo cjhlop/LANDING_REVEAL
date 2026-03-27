c277db358f2ea1b1ed35b650a08ae1df3aeda2d4
[dyad] <artem@respect.studio>
2026-03-27 18:45:21 +0200

    [dyad] Creating Home New page - wrote 3 file(s)

---
+++ src/pages/HomeNew.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import ProblemSection from "@/components/ProblemSection";
+import SolutionIntroSection from "@/components/SolutionIntroSection";
+import AudienceExplorerSection from "@/components/AudienceExplorerSection";
+import WebIDSection from "@/components/WebIDSection";
+import RadarSection from "@/components/RadarSection";
+import RevenueAttributionSection from "@/components/RevenueAttributionSection";
+import { MetricsBand } from "@/components/metrics";
+import AICopilotSection from "@/components/AICopilotSection";
+import { IntegrationsSection } from "@/components/integrations";
+import { TestimonialSection } from "@/components/testimonials";
+import { PricingSection } from "@/components/pricing-main";
+import { Footer } from "@/components/footer";
+
+const HomeNew = () => {
+  return (
+    <div className="min-h-screen bg-white font-sans antialiased">
+      <Navbar />
+      <main>
+        <Hero />
+        <ProblemSection />
+        <WebIDSection />
+        <RadarSection />
+        <AudienceExplorerSection />
+        <RevenueAttributionSection />
+        <MetricsBand />
+        <TestimonialSection />
+        <SolutionIntroSection />
+        <AICopilotSection />
+        <IntegrationsSection />
+        <PricingSection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomeNew;
