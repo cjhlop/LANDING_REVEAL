0127d06d4a490a48553c6036fd8f877dce1c9ba3
[dyad] <dudetram@gmail.com>
2026-04-08 05:35:15 +0300

    [dyad] Implementing email capture wall for additional comparisons - wrote 1 file(s)

--- src/pages/AdStrategyScanner.tsx
+++ src/pages/AdStrategyScanner.tsx
@@ -28,7 +28,8 @@ import {
   XCircle,
   Trophy,
   Mail,
-  Check
+  Check,
+  Calendar
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { showError, showSuccess } from "@/utils/toast";
