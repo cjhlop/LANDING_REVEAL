f72544016f6bf99a8d9c77b8fa2530d589bda76c
[dyad] <artem@respect.studio>
2026-06-24 18:57:47 +0300

    [dyad] Change star color to white - wrote 1 file(s)

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -20,7 +20,7 @@ const StarRating: React.FC<StarRatingProps> = ({
         <svg
           key={i}
           viewBox="0 0 24 24"
-          className={cn("h-4 w-4 fill-[#A3C7FF]", starClassName)}
+          className={cn("h-4 w-4 fill-[#FFFFFF]", starClassName)}
           aria-hidden="true"
         >
           <path d="M12 2.5l2.9 5.88 6.49.94-4.7 4.58 1.11 6.46L12 17.9l-5.8 3.05 1.11-6.46-4.7-4.58 6.49-.94L12 2.5z" />
