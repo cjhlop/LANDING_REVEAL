7cc18f86258d5b16be87918b44b0227532d450f9
[dyad] <artem@respect.studio>
2026-06-26 17:17:00 +0300

    [dyad] Set problem cards to single row - wrote 1 file(s)

--- src/pages/LinkedInAdsScheduling.tsx
+++ src/pages/LinkedInAdsScheduling.tsx
@@ -216,7 +216,7 @@ const LinkedInAdsScheduling = () => {
               </p>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-8">
               {[
                 {
                   title: "High-performing hours don't get prioritized",
